4812d6870581a41316d5c8c0a900961fe23e97c7
AhmadAlshobaki20 <ahmad.r.alshobaki@gmail.com>
2023-08-14 22:48:12 +0300

    new version

--- frontend/src/components/SideBar/SideBar.jsx
+++ frontend/src/components/SideBar/SideBar.jsx
@@ -4,39 +4,39 @@ const SideBar = () => {
   return (
     <>
       <aside className="side-bar">
-          <span>all Borders(8)</span>
+        <span>all Borders(8)</span>
+        <Link to="/" className="links">
           <div className="sidebar-item">
             <i
               class="fa-solid fa-box fa-rotate-270 fa-lg"
               style={{ color: "#bdbdbd;" }}
             ></i>
-            <Link to="/" className="links">
-              PlatformLaunch
-            </Link>
+            PlatformLaunch
           </div>
+        </Link>
+        <Link to="/market" className="links">
           <div className="sidebar-item">
             <i
               class="fa-solid fa-box fa-rotate-270 fa-lg"
               style={{ color: "#bdbdbd;" }}
             ></i>
-            <Link to="/market" className="links">
-              Marketing
-            </Link>
+            Marketing
           </div>
-          <div className="sidebar-item">
-            <i
-              class="fa-solid fa-box fa-rotate-270 fa-lg"
-              style={{ color: "#bdbdbd;" }}
-            ></i>
-            <span>Roadmap</span>
-          </div>
-          <div className="sidebar-item" id="crate">
-            <i
-              class="fa-solid fa-box fa-rotate-270 fa-lg"
-              style={{ color: "#bdbdbd;" }}
-            ></i>
-            <span>+Create New Board</span>
-          </div>
+        </Link>
+        <div className="sidebar-item">
+          <i
+            class="fa-solid fa-box fa-rotate-270 fa-lg"
+            style={{ color: "#bdbdbd;" }}
+          ></i>
+          <span>Roadmap</span>
+        </div>
+        <div className="sidebar-item" id="crate">
+          <i
+            class="fa-solid fa-box fa-rotate-270 fa-lg"
+            style={{ color: "#bdbdbd;" }}
+          ></i>
+          <span>+Create New Board</span>
+        </div>
       </aside>
     </>
   );
